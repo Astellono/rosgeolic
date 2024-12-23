98bcfe39484bf109ae1ff14e572ee10debd8a426
Astellono <94223615+Astellono@users.noreply.github.com>
2024-12-23 17:02:36 +0300

    new

--- company.php
+++ company.php
@@ -44,12 +44,12 @@ foreach ($polygons as $polygon) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+
     <title>Document</title>
     <link rel="stylesheet" href="style/reset.css">
     <script src="https://api-maps.yandex.ru/2.1/?apikey=199de9b4-eaf5-45e1-95c2-3510af592354&lang=ru_RU"
-    type="text/javascript"></script>
-    
+        type="text/javascript"></script>
+
     <!-- <link rel="stylesheet" href="style/style.css"> -->
     <script>
         let data = <?php echo json_encode($data); ?>;
@@ -57,7 +57,7 @@ foreach ($polygons as $polygon) {
         console.log(data);
     </script>
     <script defer src="js/grad.js"></script>
-    <script defer src="js/map.js"></script> 
+    <script defer src="js/map.js"></script>
     <!-- <script defer type="text/javascript">
         ymaps.ready(init);
         function init(){
@@ -67,7 +67,7 @@ foreach ($polygons as $polygon) {
             });
         }
     </script> -->
-  
+
     <link rel="stylesheet" href="style/company.css">
 </head>
 
@@ -76,88 +76,84 @@ foreach ($polygons as $polygon) {
     <div class="container">
         <h1><?= $data['name_org'] ?></h1>
 
-        <ul>
+        <ul class="info__list">
             <?php
             if ($data['sved_nedr'] != "") {
                 ?>
-                <li><span class="zag"> Сведения о пользователе недр: </span><?= $data['sved_nedr'] ?></li>
+                <li class="info__item"><span class="zag"> Сведения о пользователе недр: </span><?= $data['sved_nedr'] ?>
+                </li>
                 <?php
             }
             ?>
             <?php
             if ($data['gos_number'] != "") {
                 ?>
-                <li><span class="zag">Гос номер: </span><?= $data['gos_number'] ?></li>
+                <li class="info__item"><span class="zag">Гос номер: </span><?= $data['gos_number'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['date_gosNumber'] != "") {
                 ?>
-                <li><span class="zag">Дата присвоения гос номера: </span><?= $data['date_gosNumber'] ?></li>
+                <li class="info__item"><span class="zag">Дата присвоения гос номера: </span><?= $data['date_gosNumber'] ?>
+                </li>
                 <?php
             }
             ?>
             <?php
             if ($data['date_finish'] != "") {
                 ?>
-                <li><span class="zag">Дата окончания лицензии: </span><?= $data['date_finish'] ?></li>
+                <li class="info__item"><span class="zag">Дата окончания лицензии: </span><?= $data['date_finish'] ?></li>
                 <?php
             }
             ?>
             <?php
-            if ($data['is_elObrz']  != "") {
+            if ($data['is_elObrz'] != "") {
                 ?>
-                <li><span class="zag">Наличие электронного образца: </span><?= $data['is_elObrz'] ?></li>
+                <li class="info__item"><span class="zag">Наличие электронного образца: </span><?= $data['is_elObrz'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['for_license'] != "") {
                 ?>
-                <li><span class="zag">На что лицензии: </span><?= $data['for_license'] ?></li>
+                <li class="info__item"><span class="zag">На что лицензии: </span><?= $data['for_license'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['type_iskop'] != "") {
                 ?>
-                <li><span class="zag">Тип ископаемых: </span><?= $data['type_iskop'] ?></li>
+                <li class="info__item"><span class="zag">Тип ископаемых: </span><?= $data['type_iskop'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['name_area'] != "") {
                 ?>
-                <li><span class="zag">Название территории: </span><?= $data['name_area'] ?></li>
+                <li class="info__item"><span class="zag">Название территории: </span><?= $data['name_area'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['name_subRus'] != "") {
                 ?>
-                <li><span class="zag">Название субьекта РФ: </span><?= $data['name_subRus'] ?></li>
+                <li class="info__item"><span class="zag">Название субьекта РФ: </span><?= $data['name_subRus'] ?></li>
                 <?php
             }
             ?>
-            <?php
-            if ($data['geo_coord'] != "") {
-                ?>
-                <li><span class="zag">Географические координаты: </span><?= $data['geo_coord'] ?></li>
-                <?php
-            }
-            ?>
+
             <?php
             if ($data['status_nedr'] != "") {
                 ?>
-                <li><span class="zag">Статус недр: </span><?= $data['status_nedr'] ?></li>
+                <li class="info__item"><span class="zag">Статус недр: </span><?= $data['status_nedr'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['doc_req'] != "") {
                 ?>
-                <li><span class="zag">Реквизиты документы на основании которого выдана лицензия:
+                <li class="info__item"><span class="zag">Реквизиты документы на основании которого выдана лицензия:
                     </span><?= $data['doc_req'] ?></li>
                 <?php
             }
@@ -165,21 +161,24 @@ foreach ($polygons as $polygon) {
             <?php
             if ($data['sved_changeLicense'] != "") {
                 ?>
-                <li><span class="zag">Сведения в внесении изменений: </span><?= $data['sved_changeLicense'] ?></li>
+                <li class="info__item"><span class="zag">Сведения в внесении изменений:
+                    </span><?= $data['sved_changeLicense'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['sved_changeOform'] != "") {
                 ?>
-                <li><span class="zag">Сведения о перефоромлении лицензии: </span><?= $data['sved_changeOform'] ?></li>
+                <li class="info__item"><span class="zag">Сведения о перефоромлении лицензии:
+                    </span><?= $data['sved_changeOform'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['prikaz_req'] != "") {
                 ?>
-                <li><span class="zag">Сведения о приказа о прекращении лицензии права пользования недрами:
+                <li class="info__item"><span class="zag">Сведения о приказа о прекращении лицензии права пользования
+                        недрами:
                     </span><?= $data['prikaz_req'] ?></li>
                 <?php
             }
@@ -187,40 +186,58 @@ foreach ($polygons as $polygon) {
             <?php
             if ($data['date_stop'] != "") {
                 ?>
-                <li><span class="zag">Дата приоставнки лицензии: </span><?= $data['date_stop'] ?></li>
+                <li class="info__item"><span class="zag">Дата приоставнки лицензии: </span><?= $data['date_stop'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['date_usl'] != "") {
                 ?>
-                <li><span class="zag">Условия и сроки приостановки лицензии </span><?= $data['date_usl'] ?></li>
+                <li class="info__item"><span class="zag">Условия и сроки приостановки лицензии
+                    </span><?= $data['date_usl'] ?></li>
                 <?php
             }
             ?>
             <?php
             if ($data['sved_reestr'] != "") {
                 ?>
-                <li><span class="zag">Сведения о реестровых записях: </span><?= $data['sved_reestr'] ?></li>
+                <li class="info__item"><span class="zag">Сведения о реестровых записях: </span><?= $data['sved_reestr'] ?>
+                </li>
                 <?php
             }
             ?>
             <?php
             if ($data['link_cardLicence'] != "") {
                 ?>
-                <li><span class="zag">Ссылка на карточку лицензии:
-                <a href="<?= $data['link_cardLicence'] ?>"><?= $data['link_cardLicence'] ?></a>
+                <li class="info__item"><span class="zag">Ссылка на карточку лицензии:
+                        <a href="<?= $data['link_cardLicence'] ?>"><?= $data['link_cardLicence'] ?></a>
                 </li>
                 <?php
             }
             ?>
 
         </ul>
-        <div id="map-1" style="width: 600px; height: 400px"></div>
- 
+        <?php
+        if ($data['geo_coord'] != "") {
+            ?>
+            <hr>
+            <h2 class="coord__title">Координаты полигонов</h2>
+            <hr>
+            <div id="rootCoord" class="coordList">
+
+            </div>
+            <div id="boxMap" class="boxMap">
+
+            </div>
+            <?php
+        }
+        ?>
+
+        <!-- <div id="map-0" style="width: 600px; height: 400px"></div> -->
+
     </div>
-    
- 
+
+
 </body>
 
 </html>
--- js/map.js
+++ js/map.js
@@ -1,21 +1,70 @@
 
 
 ymaps.ready(init);
-console.log(paraCoord);
 
-function init(){
-    
-    var myMap = new ymaps.Map("map-1", {
-        center: [55.616092, 37.674804],
-        zoom: 18
+
+function init() {
+    let arrCoord = paraCoord
+    console.log(arrCoord);
+    arrCoord.forEach((e, index) => {
+        let box = document.getElementById('rootCoord')
+        let mapBox = document.getElementById('boxMap')
+        let map = document.createElement('div')
+        let btnItem = document.createElement('button')
+
+        btnItem.textContent = 'Точка-' + (index + 1)
+        btnItem.classList.add('coord__item')
+        box.classList.add('coord__list')
+
+
+        mapBox.classList.add('boxMap')
+        map.classList.add('map')
+        mapBox.append(map)
+        map.setAttribute('id', 'map-' + (index + 1))
+
+
+        // map.setAttribute('style', 'width: 600px; height: 400px')
+
+
+        var myMap = new ymaps.Map("map-" + (index + 1), {
+            center: [e.left, e.right],
+            zoom: 12
+        });
+        var myPlacemark = new ymaps.Placemark([e.left, e.right], null, {
+            preset: 'islands#blueDotIcon'
+        });
+        myMap.geoObjects.add(myPlacemark);
+
+
+
+        btnItem.setAttribute('data-id', 'map-' + (index + 1))
+        btnItem.classList.add('target__btn')
+
+
+        btnItem.addEventListener('click', (e) => {
+            // console.log(e.target.getAttribute('data-id'));
+
+            let mapArr = document.querySelectorAll('.map')
+            mapArr.forEach(el => {
+                el.style.display = 'none'
+            });
+            let activeMap = document.getElementById(e.target.getAttribute('data-id'))
+            activeMap.style.display = 'block'
+        })
+
+
+        box.append(btnItem)
+
     });
 
-    var myPlacemark = new ymaps.Placemark([55.616092, 37.674804], null, {
-        preset: 'islands#blueDotIcon'
-    });
-    myMap.geoObjects.add(myPlacemark);
-
+    let itemBtn = document.querySelectorAll('.coord__item')
+    let listBtn = document.getElementById('rootCoord')
+    console.log(listBtn);
+    console.log(itemBtn.length);
+    if (itemBtn.length < 6) {
+        listBtn.classList.add('coord__listFlexC')
+    } 
 }
-   
+
 
 
